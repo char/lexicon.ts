594e540a6563e34470d02b824568836a51ca4e33
Charlotte Som <charlotte@som.codes>
2025-05-06 18:02:45 +0100

    lex-gen for converting .json lexica to .ts modules
    also improve unconstify now we have the full atproto universe

--- lex/_util.ts
+++ lex/_util.ts
@@ -1,7 +1,10 @@
 export type Simplify<T> = {[K in keyof T]: T[K]} & {};
 
 type _WritableArray<A> =
-  A extends ReadonlyArray<infer I> ? Array<I> : A;
+    A extends readonly [] ? []
+  : A extends readonly [infer H, ...infer T] ? [Unconstify<H>, ...Unconstify<T>]
+  : A extends readonly [...infer H, infer T] ? [...Unconstify<H>, Unconstify<T>]
+  : A extends ReadonlyArray<infer I> ? Array<I> : A;
 type _WritableObject<T> =
   T extends object ? { -readonly [K in keyof T]: Unconstify<T[K]> } & {} : T;
 export type Unconstify<T> = _WritableObject<_WritableArray<T>>;
