33b4682746e798c296de1a663930abd6f4facc03
Charlotte Som <charlotte@som.codes>
2025-05-26 15:45:05 +0100

    make sure everything is exported from mod.ts

--- lib/mod.ts
+++ lib/mod.ts
@@ -1,8 +1,11 @@
 export * from "./infer.ts";
+export * from "./path.ts";
+export * from "./property.ts";
 export * from "./universe.ts";
 
 export * from "./definitions/basic.ts";
 export * from "./definitions/object.ts";
+export * from "./definitions/record.ts";
 export * from "./definitions/ref-union.ts";
 export * from "./definitions/string.ts";
 
